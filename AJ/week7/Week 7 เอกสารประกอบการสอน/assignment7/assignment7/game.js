e042434c2c83d271b825cd56f36fee9426396f73
Sura-phat <suraphat.phu@mail.kmutt.ac.th>
2023-10-02 23:40:39 +0700

    UPDATE : ASS 7 Finish

--- AJ/week7/Week 7 เอกสารประกอบการสอน/assignment7/assignment7/game.js
+++ AJ/week7/Week 7 เอกสารประกอบการสอน/assignment7/assignment7/game.js
@@ -1,75 +1,83 @@
 window.onload = pageLoad;
 
-function pageLoad() {
-    document.getElementById("start").onclick = startGame;
+function pageLoad(){
+	document.getElementById("start").onclick = startGame;
+	var x = document.getElementById('clock');
+	x.innerHTML = 30;
 }
 
-function startGame() {
-    alert("Ready");
-    addBox();
-    timeStart();
+var timer = null;
+
+function startGame(){
+	clearInterval(timer);
+	clearScreen();
+	alert("Ready");
+	addBox();
+	timeStart();
 }
 
-function timeStart() {
-    var TIMER_TICK = 1000;
-    var timer = null;
-    var min = 0.5; // 0.5 minute
-    var seconds = min * 60;
-    var x = document.getElementById('clock');
-    
-    // setting timer using setInterval function
-    timer = setInterval(timeCount, TIMER_TICK);
-    
-    function timeCount() {
-        var allbox = document.querySelectorAll("#layer div.square");
-        
-        if (allbox.length > 0) 
-		{
-            seconds--;
-            x.innerText = seconds + "s";
+function timeStart(){
+	
+	var TIMER_TICK = 1000;
+	var min = 0.5;
+	var second = min*60; 
+	var x = document.getElementById('clock');
+	console.log(timer);
+	timer = setInterval(timeCount,TIMER_TICK);
+	
+	function timeCount(){
+		var allbox = document.querySelectorAll("#layer div");
 
-            if (seconds <= 0) 
-			{
-                clearInterval(timer);
-                clearScreen();
-                alert("Game over");
-            }
-        } 	
-		else 
-		{
-            clearInterval(timer);
-            alert("You win!");
-        }
-    }
+		second--;
+		x.innerHTML = second;
+		console.log(allbox.length)
+
+		if(allbox.length === 0){
+			alert("Wingame");
+			clearScreen();
+			x.innerHTML = 30;
+			clearInterval(timer);
+		}
+		else if(second < 0){
+			alert("Lose");
+			clearScreen();
+			x.innerHTML = 30;
+			clearInterval(timer);
+		}
+	}
 }
 
-function addBox() {
-    var numbox = parseInt(document.getElementById("numbox").value) || 4;
-    var gameLayer = document.getElementById('layer');
-    var colorDrop = document.getElementById("color").value || "maroon";
-    
-    for (var i = 0; i < numbox; i++) {
-        var tempbox = document.createElement('div');
-        tempbox.className = 'square ' + colorDrop;
-        tempbox.id = "box" + i;
-        tempbox.style.left = Math.random() * (500 - 25) + "px";
-        tempbox.style.top = Math.random() * (500 - 25) + "px";
-        
-        gameLayer.appendChild(tempbox);
-        bindBox(tempbox);
-    }
+
+function addBox(){
+	var numbox = document.getElementById("numbox").value;
+	var gameLayer = document.getElementById("layer");
+	var colorDrop = document.getElementById("color").value;
+    console.log(gameLayer);
+	
+    for (var i =0; i<numbox;i++){
+		var tempbox = document.createElement('div');
+		tempbox.className = "square";
+		tempbox.id = "box"+i;
+		tempbox.style.left = Math.random() * (500 - 25) + "px";
+		tempbox.style.top = Math.random() * (500 - 25) + "px";
+		tempbox.style.backgroundColor = colorDrop;
+		gameLayer.appendChild(tempbox);
+		bindBox(tempbox);
+	}
 }
 
-function bindBox(box) {
-    box.onclick = function () {
-        this.style.display = 'none';
-    };
+function bindBox(box){
+	box.onclick = box.remove;
 }
 
-function clearScreen() {
-    var allbox = document.querySelectorAll("#layer div.square");
-
-    for (var i = 0; i < allbox.length; i++) {
-        allbox[i].remove();
-    }
+function clearScreen(){
+	var allbox = 0;
+	var allbox = document.querySelectorAll("#layer div");
+	
+    for (var i=0;i<allbox.length;i++){
+		allbox[i].remove();
+	}
 }
+
+
+
